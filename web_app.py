bd901966217722c521be0c75bae03b0b732bef47
Maze226 <michaelgaragaty@gmail.com>
2023-02-08 20:19:15 +0200

    created basic web_app

--- web_app.py
+++ web_app.py
@@ -1,1 +1,12 @@
-print('Hi, Jenkins!')
+from flask import Flask
+
+app = Flask('__main__')
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello World</h1>'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
